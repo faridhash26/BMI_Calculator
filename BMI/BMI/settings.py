a45a7380b56c23ed357fdd7ff737c6c73e9fbb50
f.hashemiyan <f.hashemiyan@taraz.org>
2022-04-22 21:51:58 +0430

    fixing the django filter problem

--- BMI/BMI/settings.py
+++ BMI/BMI/settings.py
@@ -137,6 +137,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'user.BMIUser'
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend', 'rest_framework.filters.SearchFilter'),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
--- BMI/bmi_backend_app/filters.py
+++ BMI/bmi_backend_app/filters.py
@@ -1,15 +1,17 @@
 from django_filters import rest_framework as filters
 from django.db.models import Q
 
-
 from .models import UserBmiInfo
+
 
 def filter_by_status_types(queryset, name, value):
     values = value.split(',')
     return queryset.filter(status__in=values)
 
+
 class BMIInfoFilter(filters.FilterSet):
     status_type = filters.CharFilter(method=filter_by_status_types)
+
     class Meta:
         model = UserBmiInfo
         fields = ['status_type']
